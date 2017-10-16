MoKee/android_device_oppo_common

configpanel/keyhandler: slider: Add ringer control
Change-Id: Iac9a8431ea6f93787dcbec62263b213ca3c4668d

keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -50,6 +50,7 @@ import org.mokee.settings.device.slider.NotificationController;
 import org.mokee.settings.device.slider.FlashlightController;
 import org.mokee.settings.device.slider.BrightnessController;
 import org.mokee.settings.device.slider.RotationController;
+import org.mokee.settings.device.slider.RingerController;
 
 public class KeyHandler implements DeviceKeyHandler {
 
@@ -82,6 +83,7 @@ public class KeyHandler implements DeviceKeyHandler {
     private final FlashlightController mFlashlightController;
     private final BrightnessController mBrightnessController;
     private final RotationController mRotationController;
+    private final RingerController mRingerController;
 
     private SliderControllerBase mSliderController;
 
@@ -113,6 +115,10 @@ public class KeyHandler implements DeviceKeyHandler {
                     break;
                 case RotationController.ID:
                     mSliderController = mRotationController;
+                    mSliderController.update(actions);
+                    break;
+                case RingerController.ID:
+                    mSliderController = mRingerController;
                     mSliderController.update(actions);
                     break;
             }
@@ -150,6 +156,7 @@ public class KeyHandler implements DeviceKeyHandler {
         mFlashlightController = new FlashlightController(context);
         mBrightnessController = new BrightnessController(context);
         mRotationController = new RotationController(context);
+        mRingerController = new RingerController(context);
 
         mContext.registerReceiver(mUpdateReceiver,
                 new IntentFilter(ACTION_UPDATE_SLIDER_SETTINGS));

keyhandler/src/org/mokee/settings/device/slider/RingerController.java
@@ -1,0 +1,65 @@
+/*
+ * Copyright (C) 2017 The MoKee Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mokee.settings.device.slider;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.util.Log;
+import android.util.SparseIntArray;
+
+import org.mokee.settings.device.SliderControllerBase;
+
+public final class RingerController extends SliderControllerBase {
+
+    public static final int ID = 5;
+
+    private static final String TAG = "RingerController";
+
+    private static final int RINGER_NORMAL = 50;
+    private static final int RINGER_VIGRATE = 51;
+    private static final int RINGER_SILENT = 52;
+
+    private static final SparseIntArray MODES = new SparseIntArray();
+    static {
+        MODES.put(RINGER_NORMAL, AudioManager.RINGER_MODE_NORMAL);
+        MODES.put(RINGER_VIGRATE, AudioManager.RINGER_MODE_VIBRATE);
+        MODES.put(RINGER_SILENT, AudioManager.RINGER_MODE_SILENT);
+    }
+
+    private final AudioManager mAudioManager;
+
+    public RingerController(Context context) {
+        super(context);
+        mAudioManager = getSystemService(Context.AUDIO_SERVICE);
+    }
+
+    @Override
+    protected boolean processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        if (MODES.indexOfKey(action) >= 0) {
+            mAudioManager.setRingerModeInternal(MODES.get(action));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void reset() {
+        // do nothing, just leave the phone in the current mode
+    }
+}

mukesh22584/device_oppo_common

keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015-2016 The CyanogenMod Project
- *               2017 The cyanogenmod Open Source Project
+ *               2017 The MoKee Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,6 +50,7 @@ import org.cyanogenmod.settings.device.slider.NotificationController;
 import org.cyanogenmod.settings.device.slider.FlashlightController;
 import org.cyanogenmod.settings.device.slider.BrightnessController;
 import org.cyanogenmod.settings.device.slider.RotationController;
+import org.cyanogenmod.settings.device.slider.RingerController;
 
 public class KeyHandler implements DeviceKeyHandler {
 
@@ -82,6 +83,7 @@ public class KeyHandler implements DeviceKeyHandler {
     private final FlashlightController mFlashlightController;
     private final BrightnessController mBrightnessController;
     private final RotationController mRotationController;
+    private final RingerController mRingerController;
 
     private SliderControllerBase mSliderController;
 
@@ -113,6 +115,10 @@ public class KeyHandler implements DeviceKeyHandler {
                     break;
                 case RotationController.ID:
                     mSliderController = mRotationController;
+                    mSliderController.update(actions);
+                    break;
+                case RingerController.ID:
+                    mSliderController = mRingerController;
                     mSliderController.update(actions);
                     break;
             }
@@ -150,6 +156,7 @@ public class KeyHandler implements DeviceKeyHandler {
         mFlashlightController = new FlashlightController(context);
         mBrightnessController = new BrightnessController(context);
         mRotationController = new RotationController(context);
+        mRingerController = new RingerController(context);
 
         mContext.registerReceiver(mUpdateReceiver,
                 new IntentFilter(ACTION_UPDATE_SLIDER_SETTINGS));

keyhandler/src/org/cyanogenmod/settings/device/slider/RingerController.java
@@ -1,0 +1,65 @@
+/*
+ * Copyright (C) 2017 The MoKee Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.settings.device.slider;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.util.Log;
+import android.util.SparseIntArray;
+
+import org.cyanogenmod.settings.device.SliderControllerBase;
+
+public final class RingerController extends SliderControllerBase {
+
+    public static final int ID = 5;
+
+    private static final String TAG = "RingerController";
+
+    private static final int RINGER_NORMAL = 50;
+    private static final int RINGER_VIGRATE = 51;
+    private static final int RINGER_SILENT = 52;
+
+    private static final SparseIntArray MODES = new SparseIntArray();
+    static {
+        MODES.put(RINGER_NORMAL, AudioManager.RINGER_MODE_NORMAL);
+        MODES.put(RINGER_VIGRATE, AudioManager.RINGER_MODE_VIBRATE);
+        MODES.put(RINGER_SILENT, AudioManager.RINGER_MODE_SILENT);
+    }
+
+    private final AudioManager mAudioManager;
+
+    public RingerController(Context context) {
+        super(context);
+        mAudioManager = getSystemService(Context.AUDIO_SERVICE);
+    }
+
+    @Override
+    protected boolean processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        if (MODES.indexOfKey(action) >= 0) {
+            mAudioManager.setRingerModeInternal(MODES.get(action));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void reset() {
+        // do nothing, just leave the phone in the current mode
+    }
+}